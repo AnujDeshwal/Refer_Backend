88722e8a724723f2f09e4ea664b825da2581a384
AnujDeshwal <anujdeshwal95@gmail.com>
2024-07-10 00:15:07 +0530

    config updated

--- utility/config.js
+++ utility/config.js
@@ -2,7 +2,7 @@ exports.corsOptions = {
     origin: [
       "http://localhost:5174",
       "http://localhost:5173",
-      "https://refer-frontend.vercel.app/"
+      "https://refer-frontend.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
